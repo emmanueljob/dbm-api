835f9c292fb90302007b71aea788e712a9b6080e
Emmanuel Job <emmanuel.job@accuenmedia.com>
2015-04-02 18:22:01 +0000

    add some helper function

--- dbmclient/models/base.py
+++ dbmclient/models/base.py
@@ -9,3 +9,18 @@ class Base(dict):
     def __init__(self, connection):
         Base.connection = connection
         super(Base, self).__init__()
+
+    def encode_for_id(self, id):
+        # TERRIBLE!!!!! BUT GOOGLE DOESNT SEND US AN ID.
+        # remove '=' because it messes up urls. we never decode so its not a big deal.
+        id = base64.b64encode(id).strip('=')
+
+    def decode_id(self, id):
+        # TERRIBLE!!!!! BUT GOOGLE DOESNT SEND US AN ID.
+        # remove '=' because it messes up urls. we never decode so its not a big deal.
+        
+        missing_padding = 4 - len(id) % 4
+        if missing_padding:
+            id += b'='* missing_padding
+        id = base64.b64encode(id).strip('=')
+        return id
