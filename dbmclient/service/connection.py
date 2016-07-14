b8e16820f30c43e0630ad8853d25b7c5451688ad
vagrant <vagrant@vagrant.vm>
2016-07-14 18:24:55 +0000

    add try/catch for p12

--- dbmclient/service/connection.py
+++ dbmclient/service/connection.py
@@ -6,21 +6,27 @@ import base64
 from oauth2client.service_account import ServiceAccountCredentials
 import httplib2
 
+class DBMP12FileDoesNotExist(Exception):
+    pass
+
 class Connection:
 
     authorization_token = None
     # use Accuen DBM as the p12, NOT API Project!
     # key_file = os.path.dirname(os.path.realpath(__file__)) + "/Accuen-DBM-9cf79343fc42.p12"
 
-    key_file = os.environ['P12FILEDBM']
-
     def __init__(self, p12=None):
         Connection.username = '342021153007-se0r0lv1a0sckq581ko2cnkre12cce9b@developer.gserviceaccount.com'
-        if p12:
-            self.key_file = p12
-
-        with open(self.key_file) as f:
-            Connection.password = f.read()
+        try:
+            if p12:
+                self.key_file = p12
+            else:
+                key_file = os.environ['P12FILEDBM']
+            
+            with open(self.key_file) as f:
+                Connection.password = f.read()
+        except:
+            raise DBMP12FileDoesNotExist
 
     def connect(self):
         Connection.get_authorization()
@@ -33,7 +39,6 @@ class Connection:
     def authorize(self):
 
         credentials = ServiceAccountCredentials.from_p12_keyfile(Connection.username, self.key_file, scopes=['https://www.googleapis.com/auth/doubleclickbidmanager'])
-
         _API_VERSION = 'v1'
 
         # Create an httplib2.Http object to handle our HTTP requests and authorize it
