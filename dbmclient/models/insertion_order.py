02c9bdb11848ecc2631b92a1d8d3d2386f4a1719
Matt Artingstall <matt@imagndesign.com>
2017-06-20 15:44:22 -0700

    Use new SDF file

--- dbmclient/models/insertion_order.py
+++ dbmclient/models/insertion_order.py
@@ -12,6 +12,67 @@ class InsertionOrder(Base):
         service = self.get_service()
 
         try:
+            request_body = {
+                'filterType': 'ADVERTISER_ID',
+                'filterIds': [advertiser_id],
+                'fileTypes': ['INSERTION_ORDER']
+            }
+            request = service.sdf().download(body=request_body)
+            response = request.execute()
+
+        except client.AccessTokenRefreshError:
+            print ("The credentials have been revoked or expired, please re-run"
+                   "the application to re-authorize")
+
+        if 'insertionOrders' not in resp:
+            return None
+
+        ios = resp['insertionOrders']
+        first = True
+        rval = []
+        ids = []
+        for raw_io in csv.reader(ios.encode('utf-8').split('\n')):
+            if len(raw_io) == 0:
+                continue
+
+            if first:
+                first = False
+                continue
+
+            insertionOrder = InsertionOrder(InsertionOrder.connection)
+            id = self.encode_for_id(raw_lineitem[0])
+            if id in ids:
+                continue
+
+            ids.append(id)
+            insertionOrder['id'] = id
+            insertionOrder['name'] = raw_lineitem[1]
+            insertionOrder['advertiser_name'] = ''
+
+            budget_segments = raw_lineitem[20]
+            budget_segments = budget_segments.replace('(', '')
+            budget_segments = budget_segments.split(');')
+
+            budget = 0.0
+            for budget_segment in budget_segments:
+                segment = budget_segment.split(';')
+                budget += int(segment[0].lstrip())
+
+            insertionOrder['budget'] = budget
+
+            start_date = budget_segments[0].split(';')
+            insertionOrder['start_date'] = start_date[1].lstrip())
+            
+            end_date = budget_segments[(budget_segments - 1)].split(';')
+            insertionOrder['end_date'] = start_date[2].lstrip())
+            rval.append(insertionOrder)
+
+        return rval
+
+    def find_by_advertiser_old(self, advertiser_id):
+        service = self.get_service()
+
+        try:
             body = { 'filterType': 'ADVERTISER_ID', 'filterIds': [advertiser_id]}
 
             req = service.lineitems().downloadlineitems(body=body)
