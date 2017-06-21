04e085c1db8518490266eb7c324427c01fb7799b
Matt Artingstall <matt@imagndesign.com>
2017-06-20 18:09:50 -0700

    Fixes to get start/end dates

--- dbmclient/models/insertion_order.py
+++ dbmclient/models/insertion_order.py
@@ -52,23 +52,23 @@ class InsertionOrder(Base):
 
             budget_segments = raw_io[20]
             budget_segments = budget_segments.replace('(', '')
-            budget_segments = budget_segments.split(');')
+            budget_segments = budget_segments.split('); ')
 
             budget = 0.0
             for budget_segment in budget_segments:
                 if budget_segment:
-                    segment = budget_segment.split(';')
+                    segment = budget_segment.split('; ')
                     if segment[0] != '':
                         value = segment[0].strip('" ').lstrip()
                         budget += float(value)
 
             insertionOrder['budget'] = float(budget)
 
-            start_date = budget_segments[0].split(';')
+            start_date = budget_segments[0].split('; ')
             insertionOrder['start_date'] = start_date[1].lstrip()
-            
-            end_date = budget_segments[(len(budget_segments) - 1)].split(';')
-            insertionOrder['end_date'] = start_date[2].lstrip()
+
+            end_date = budget_segments[(len(budget_segments) - 1)].split('; ')
+            insertionOrder['end_date'] = end_date[2].lstrip()
             rval.append(insertionOrder)
 
         return rval
